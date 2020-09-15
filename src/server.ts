a786adf005f5588e6f2d8589078936f81da5be50
unknown <viniciusbeloti7@gmail.com>
2020-09-15 17:04:55 -0300

    feat: add middleware global error

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,33 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 
 import './database';
 import { tmpFolder } from './config/upload';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(tmpFolder));
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🔥 Server started 🔥');
