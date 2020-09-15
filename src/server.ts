80099c39bc2c7c978e0e7f826f8a612ad303f674
unknown <viniciusbeloti7@gmail.com>
2020-09-15 10:43:35 -0300

    feat: add eslint

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ ok: true });
-})
+app.get('/', (req, res) => res.json({ ok: true }));
 
 app.listen(3333, () => {
-    console.log('🔥 Server started 🔥');
+  console.log('🔥 Server started 🔥');
 });
