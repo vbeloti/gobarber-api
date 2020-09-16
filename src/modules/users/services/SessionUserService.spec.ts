dc1d7e22c2ee7ee8cce511dbc02a1fc50adf6c96
unknown <viniciusbeloti7@gmail.com>
2020-09-16 20:29:36 -0300

    test: ensure not be able to authenticate with wrong

--- src/modules/users/services/SessionUserService.spec.ts
+++ src/modules/users/services/SessionUserService.spec.ts
@@ -33,4 +33,49 @@ describe('SessionUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
   });
+
+  it('should be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const sessionUser = new SessionUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      sessionUser.execute({
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const sessionUser = new SessionUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+
+    expect(
+      sessionUser.execute({
+        email: 'any_email@mail.com',
+        password: 'wrong_password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
