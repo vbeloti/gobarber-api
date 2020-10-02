c3509f15c703184524b6db41faa4c0f0fe18d079
unknown <viniciusbeloti7@gmail.com>
2020-10-02 11:19:31 -0300

    feat: add dontenv

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -46,7 +46,7 @@ class SendForgotPassowrdEmailService {
         file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          link: `http://localhost:3000/reset_password?token=${token}`,
+          link: `${process.env.APP_WEB_UR}/reset_password?token=${token}`,
         },
       },
     });
