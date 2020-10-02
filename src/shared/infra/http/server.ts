eb54f2ae4fff1de4292d86ebe8239b3f0892294f
unknown <viniciusbeloti7@gmail.com>
2020-10-02 10:39:58 -0300

    feat: add celebrate to valid routes fields

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 import cors from 'cors';
 
 import { tmpFolder } from '@config/upload';
@@ -18,6 +19,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(tmpFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   if (err instanceof AppError) {
