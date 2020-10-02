43ffdc2b7e700d7fea7368855adf675091a6a426
unknown <viniciusbeloti7@gmail.com>
2020-10-02 15:09:58 -0300

    feat: add rate limiter flexible

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import redis from 'redis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import AppError from '@shared/errors/AppError';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS || undefined,
+  enable_offline_queue: false,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'rateLimit',
+  points: 5,
+  duration: 5,
+});
+
+export default async function (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(req.ip);
+
+    return next();
+  } catch (error) {
+    throw new AppError('Too many requests', 429);
+  }
+}
